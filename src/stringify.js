3cf30b239b0e9ccbbfd9991aa054af2162efcb8e
Honigstein Samuel <samuel.honigstein@gmail.com>
2016-01-22 16:52:20 +0000

    Added command line support for extensions

--- src/stringify.js
+++ src/stringify.js
@@ -70,11 +70,11 @@ function getExtensions (options) {
   if (options) {
     if (Object.prototype.toString.call(options) === '[object Array]') {
       extensions = options;
-    } else if (options.extensions) {
-      extensions = options.extensions;
+    } else if (options.extensions && options.extensions._) {
+      extensions = options.extensions._;
     }
-    else if( options.exts ) {
-      extensions = options.exts._;
+    else if(options.extensions) { 
+      extensions = options.extensions
     }
   }
 
